d96ad1c9b148f2dcf76a14316d4af4235380839f
Ouarich Inas <inasouarich@gmail.com>
2026-03-25 15:00:26 +0100

    js

---
+++ src/js - Copie/lose.js
@@ -0,0 +1,57 @@
+
+export default class lose extends Phaser.Scene {
+    // constructeur de la classe
+    constructor() {
+        super({
+            key: "lose" //  ici on précise le nom de la classe en tant qu'identifiant
+        });
+
+    }
+
+    preload() {
+
+    }
+
+    create(){
+
+    const screen_win = this.add.image(640, 384, "screen_lose"); // Réglez la valeur selon vos besoins
+
+    const button_back = this.add.image(640, 630, "button_back"); // Réglez la valeur selon vos besoins
+    
+
+    
+    button_back.setInteractive();    
+    button_back.on("pointerover", () => {
+      button_back.setScale(1.1);
+      button_back.setTint(0xC0C0C0);
+    });
+    button_back.on("pointerout", () => {
+      button_back.setScale(1.0);
+      button_back.clearTint();
+    });
+
+    button_back.on("pointerup", () => {
+      this.scene.start("accueil");
+    });
+
+this.sound.stopByKey('son_game');
+
+ if (!this.sound.get('son_game_over')) {
+      console.warn('Sound "son_game_over" not found in the sound manager.');
+     
+    }
+    else {
+    // Play the sound for the win screen
+    this.sound.play('son_game_over', {
+        loop: false
+    });
+    }
+    
+    }
+
+
+    udpate() {
+
+    }
+
+}
